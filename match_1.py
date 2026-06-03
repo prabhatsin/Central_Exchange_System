5e3c5585444dfd777d40543b0018de3c429e5519
prabhatsin <prabhat.singh112244@gmail.com>
2026-06-03 06:46:41 +0530

    BUY PART SPOT MARKETorder

---
+++ match_1.py
@@ -0,0 +1,13 @@
+
+#! This Script contains the matching logic of  'BUY' Part of a SPOT 'MARKET' Order 
+
+incoming_request={
+    'userId': 23,
+    'stockId':7,
+    'price':None,
+    'qty':5,
+    'side':'BUY',
+    'type': 'MARKET'
+
+}
+
